b9924c7ed3ae5b0eb5b5d9865bf8774b3e333cfe
MatthewSDick <matthew.dick@outlook.com>
2020-05-28 17:23:37 -0400

    Finished Sorted Rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,22 +1,22 @@
 import React from "react";
 import Room from "./Room";
-
-export default function RoomList({ rooms }) {
+const RoomsList = ({ rooms }) => {
   if (rooms.length === 0) {
     return (
       <div className="empty-search">
-        <h3>Unfortunately no rooms match your search parameters</h3>
+        <h3>unfortunately no rooms matched your search parameters</h3>
       </div>
     );
   }
-
   return (
     <section className="roomslist">
-      <div className="roomlist-center">
+      <div className="roomslist-center">
         {rooms.map((item) => {
           return <Room key={item.id} room={item} />;
         })}
       </div>
     </section>
   );
-}
+};
+
+export default RoomsList;
