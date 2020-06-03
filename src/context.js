42b192d27a0bc63661aee16e4245b20bafe350be
MatthewSDick <matthew.dick@outlook.com>
2020-06-03 10:54:52 -0400

    Added proce range slider

--- src/context.js
+++ src/context.js
@@ -55,15 +55,45 @@ class RoomProvider extends Component {
   };
 
   handleChange = (event) => {
-    const type = event.target.type;
+    // const type = event.target.type;
+    const target = event.target;
+    // const value = event.target.value;
+    const value = event.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
-    const value = event.target.value;
 
-    console.log(type, name, value);
+    this.setState(
+      {
+        [name]: value,
+      },
+      this.filterRooms
+    );
   };
 
   filterRooms = () => {
-    console.log("hello");
+    let { rooms, type, capacity, price, minSize, maxSize, breakfast, pets } = this.state;
+
+    // all the rooms
+    let tempRooms = [...rooms];
+    // transform values
+    capacity = parseInt(capacity);
+    price = parseInt(price);
+
+    // filter by type
+    if (type !== "all") {
+      tempRooms = tempRooms.filter((room) => room.type === type);
+    }
+
+    // filter by capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter((room) => room.capacity >= capacity);
+    }
+
+    // filter by price
+    tempRooms = tempRooms.filter((room) => room.price <= price);
+
+    this.setState({
+      sortedRooms: tempRooms,
+    });
   };
 
   render() {
